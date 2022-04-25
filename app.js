90df82d5ed8b0e1cc0868509cb8fa09c0eafd346
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-04-25 14:32:06 +0530

    new route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send('Its worked')
+});
+
+app.get('/new',(req,res)=>{
+    res.send('Its new is worked')
 })
 app.listen(process.env.PORT || 5000,()=>{
     console.log('server is connected')
